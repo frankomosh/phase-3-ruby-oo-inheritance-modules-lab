154bc5d36e86d0bf6c3eefdf4d0cfabc1f1f5398
Frank-197 <frankomosh197@gmail.com>
2022-08-20 16:15:20 +0300

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,31 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
-
+  @@all = []
+ 
   def initialize
-    @@artists << self
+    @@all << self 
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+ def self.all
+    @@all
+  end  
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
-
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+ 
+  def add_songs(songs) 
+    songs.each { |song| add_song(song) } 
   end
 end
+ 
---
+++ lib/concerns/findable.rb
@@ -0,0 +1,13 @@
+module Findable
+    module ClassMethods
+        def find_by_name(name)
+            self.all.detect{|a| a.name == name}
+        end
+
+        def to_param
+            name.downcase.gsub(' ', '-')
+        end
+    end
+end
+
+
